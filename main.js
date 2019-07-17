d6c39f5ac1bfda48bca14a88949513e345542bff
Uri Kalish <uri.kalish@microfocus.com>
2019-07-17 19:14:34 +0300

    wip

--- main.js
+++ main.js
@@ -1,31 +1,96 @@
-const getFrame = (frameIndex) => {
-	return document.getElementById(`frame${frameIndex}`);
+const initViews = () => {
+	settings.views.forEach(v => {
+		v.kind = v.kind || settings.defaults.kind;
+		v.displayTime = v.displayTime || settings.defaults.displayTime;
+		v.width = v.width || settings.defaults.width;
+		v.height = v.height || settings.defaults.height;
+	});
 };
-const loadFrame = (frameIndex, viewIndex) => {
-	const frame = getFrame(frameIndex);
-	frame.src = settings.views[viewIndex].src;
+const loadText = (view) => {
+	const elm = document.createElement('div');
+	elm.style.width = view.width + 'px';
+	elm.style.height = view.height + 'px';
+	if (view.textBig) {
+		const textBigElm = document.createElement('div');
+		textBigElm.classList.add('big');
+		textBigElm.innerText = view.textBig;
+		elm.appendChild(textBigElm);
+	}
+	if (view.textSmall) {
+		const textSmallElm = document.createElement('div');
+		textSmallElm.classList.add('small');
+		textSmallElm.innerText = view.textSmall;
+		elm.appendChild(textSmallElm);
+	}
+	document.getElementById('main').appendChild(elm);
+	return elm;
 };
-const hideFrame = (frameIndex) => {
-	const frame = getFrame(frameIndex);
-	frame.classList.remove('show');
-	frame.classList.add('hide');
+const loadImage = (view) => {
+	const elm = document.createElement('img');
+	elm.style.width = view.width + 'px';
+	elm.style.height = view.height + 'px';
+	elm.src = view.src;
+	document.getElementById('main').appendChild(elm);
+	return elm;
 };
-const showFrame = (frameIndex) => {
-	const frame = getFrame(frameIndex);
-	frame.classList.remove('hide');
-	frame.classList.add('show');
+const loadFrame = (view) => {
+	const elm = document.createElement('iframe');
+	const w = view.width;
+	const h = view.height;
+	elm.setAttribute('width', w + 'px');
+	elm.setAttribute('height', h + 'px');
+	elm.setAttribute('scrolling', 'no');
+	elm.setAttribute('frameborder', '0');
+	elm.setAttribute('allowfullscreen', 'true');
+	elm.src = view.src || 'about:blank';
+	document.getElementById('main').appendChild(elm);
+	return elm;
 };
-const toggleFrame = (frameIndex, viewIndex) => {
-	const otherFrameIndex = frameIndex === 0 ? 1 : 0;
-	const otherViewIndex = (viewIndex + 1) % settings.views.length;
-	showFrame(frameIndex);
-	hideFrame(otherFrameIndex);
+const loadView = (ind) => {
+	const obj = {
+		ind: ind,
+		view: settings.views[ind]
+	};
+	switch (obj.view.kind) {
+		case 'text': {
+			obj.elm = loadText(obj.view);
+			break;
+		}
+		case 'image': {
+			obj.elm = loadImage(obj.view);
+			break;
+		}
+		case 'frame':
+		default: {
+			obj.elm = loadFrame(obj.view);
+			break;
+		}
+	}
+	obj.elm.classList.add('hide');
+	obj.elm.classList.add('view');
+	obj.elm.classList.add('view-' + obj.view.kind);
+	const w = obj.view.width;
+	const h = obj.view.height;
+	obj.elm.style['left'] = `calc(50vw - ${w/2}px`;
+	obj.elm.style['top'] = `calc(50vh - ${h/2}px`;
+	return obj;
+};
+const toggleView = (oldView, newView) => {
+	newView.elm.classList.remove('hide');
+	newView.elm.classList.add('show');
+	if (oldView) {
+		oldView.elm.classList.remove('show');
+		oldView.elm.classList.add('hide');
+		setTimeout(() => {
+			oldView.elm.parentNode.removeChild(oldView.elm);
+		}, 2000);
+	}
 	setTimeout(() => {
-		loadFrame(otherFrameIndex, otherViewIndex)},
-		1000);
-	setTimeout(() => {
-		toggleFrame(otherFrameIndex, otherViewIndex)},
-		(settings.views[viewIndex].displayTime || settings.defaultViewDisplayTime) * 1000);
+		const nextView = loadView((newView.ind + 1) % settings.views.length);
+		setTimeout(() => {
+			toggleView(newView, nextView)},
+			newView.view.displayTime * 1000);
+	}, 1000);
 };
-loadFrame(0, 0);
-setTimeout(() => {toggleFrame(0, 0);}, 0);
+initViews();
+toggleView(null, loadView(0));
--- settings.js
+++ settings.js
@@ -1,17 +1,33 @@
 const settings = {
-	defaultViewDisplayTime: 10,
+	defaults: {
+		kind: 'frame',
+		displayTime: 10,
+		width: 1920,
+		height: 1200,
+	},
 	views: [
 		{
-			src: "res/img1.jpg",
-			displayTime: 5
+			kind: 'text',
+			textBig: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			textSmall: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+			width: 1000,
+			height: 500,
+			displayTime: 50000,
 		},
 		{
-			src: "http://caniuse.com",
-			displayTime: 5
+			kind: 'image',
+			src: 'res/img1.jpg',
+			displayTime: 5,
+			width: 769,
+			height: 960,
 		},
 		{
-			src: "http://ynet.co.il",
-			displayTime: 5
+			src: 'http://caniuse.com',
+			displayTime: 5,
+		},
+		{
+			src: 'https://www.caniuse.com/#search=grid',
+			displayTime: 5,
 		},
 	]
 };
