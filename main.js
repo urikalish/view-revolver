2f27c9af95f33a541b22136c08814550d370f017
Uri Kalish <uri.kalish@microfocus.com>
2019-07-17 20:54:06 +0300

    wip

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const initViews = () => {
-	settings.views.forEach(v => {
-		v.kind = v.kind || settings.defaults.kind;
-		v.displayTime = v.displayTime || settings.defaults.displayTime;
-		v.width = v.width || settings.defaults.width;
-		v.height = v.height || settings.defaults.height;
-	});
-};
 const showView = (view) => {
 	view.containerElm.classList.remove('hide');
 	view.containerElm.classList.add('show');
@@ -16,8 +8,6 @@ const hideView = (view) => {
 };
 const createTextElm = (view) => {
 	const elm = document.createElement('div');
-	elm.style.width = view.width + 'px';
-	elm.style.height = view.height + 'px';
 	if (view.textBig) {
 		const textBigElm = document.createElement('div');
 		textBigElm.classList.add('big');
@@ -34,20 +24,20 @@ const createTextElm = (view) => {
 };
 const createImageElm = (view) => {
 	const elm = document.createElement('img');
-	elm.style.width = view.width + 'px';
-	elm.style.height = view.height + 'px';
 	elm.src = view.src;
 	return elm;
 };
 const createFrameElm = (view) => {
 	const elm = document.createElement('iframe');
-	const w = view.width;
-	const h = view.height;
-	elm.setAttribute('width', w + 'px');
-	elm.setAttribute('height', h + 'px');
 	elm.setAttribute('scrolling', 'no');
 	elm.setAttribute('frameborder', '0');
 	elm.setAttribute('allowfullscreen', 'true');
+	const w = settings.frameWidth;
+	const h = settings.frameHeight;
+	elm.setAttribute('width', w + 'px');
+	elm.setAttribute('height', h + 'px');
+	elm.style['left'] = `calc(50vw - ${w/2}px`;
+	elm.style['top'] = `calc(50vh - ${h/2}px`;
 	elm.src = view.src || 'about:blank';
 	return elm;
 };
@@ -73,10 +63,6 @@ const loadView = (ind) => {
 	}
 	obj.elm.classList.add('view');
 	obj.elm.classList.add('view-' + obj.view.kind);
-	const w = obj.view.width;
-	const h = obj.view.height;
-	obj.elm.style['left'] = `calc(50vw - ${w/2}px`;
-	obj.elm.style['top'] = `calc(50vh - ${h/2}px`;
 	obj.containerElm = document.createElement('div');
 	obj.containerElm.classList.add('view-container');
 	obj.containerElm.classList.add('view-container-' + obj.view.kind);
@@ -100,6 +86,5 @@ const toggleView = (oldView, newView) => {
 			newView.view.displayTime * 1000);
 	}, 1000);
 };
-initViews();
 const firstView = loadView(0);
 toggleView(null, firstView);
--- settings.js
+++ settings.js
@@ -1,32 +1,26 @@
 const settings = {
-	defaults: {
-		kind: 'frame',
-		displayTime: 10,
-		width: 1920,
-		height: 1200,
-	},
+	frameWidth: 1920,
+	frameHeight: 1200,
 	views: [
 		{
-			kind: 'text',
 			textBig: 'Lorem ipsum dolor sit amet',
 			textSmall: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-			width: 1000,
-			height: 300,
+			kind: 'text',
 			displayTime: 5,
 		},
 		{
-			kind: 'image',
 			src: 'res/img1.jpg',
+			kind: 'image',
 			displayTime: 5,
-			width: 769,
-			height: 960,
 		},
 		{
 			src: 'http://caniuse.com',
+			kind: 'frame',
 			displayTime: 5,
 		},
 		{
 			src: 'https://www.caniuse.com/#search=grid',
+			kind: 'frame',
 			displayTime: 5,
 		},
 	]
